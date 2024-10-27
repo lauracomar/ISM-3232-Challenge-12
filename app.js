d7c6368b905168c36ef905e9873cccf139320d81
Laura Comar <lautmcomar@gmail.com>
2024-10-27 16:21:56 -0400

    Implement color selection and canvas clearing functionality (fixed issues)

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ canvas.addEventListener('mousedown', (e) => {
 // move mouse - draw shapes
 canvas.addEventListener('mousemove', (e) => {
     if (!drawing) return; // if statement to leave function if not drawing
-    context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
     drawShape(e.offsetX, e.offsetY); // call drawShape to draw selected shape
 });
 
 // mosue up - stop drawing
 canvas.addEventListener('mouseup', () => {
-    drawing = false; // 
+    drawing = false;
+    drawShape(e.offsetX, e.offsetY);
 });
 
 // mouse out - stop drawing when the mouse leaves the canvas
@@ -35,9 +35,9 @@ canvas.addEventListener('mouseout', () => {
 
 // Task 3: Implement Shape Drawing Logic
 function drawShape(X, Y) {
-    const shape = document.querySelector('input[name="shape"]: checked').value; //get selected shape
+    const shape = document.querySelector('input[name="shape"]:checked').value; //get selected shape
     context.beginPath();// new path 
-    context.strokeStyle = colorPicker.value; // set stroke to color chosen
+    context.strokeStyle = colorInput.value; // set stroke to color chosen
 
     if (shape === 'line') {
         context.moveTo(startX, startY); //move to starting point
@@ -49,4 +49,10 @@ function drawShape(X, Y) {
         context.arc(startX, startY, radius, 0, Math.PI * 2);//draw circle
     }
     context.stroke();//stroke to draw
+}
+
+//task 4: Add Color Selection and Canvas Clearing
+clearButton.addEventListener('click', clearCanva)// clear button
+function clearCanva() { //function to clear canva
+    context.clearRect(0, 0, canvas.width, canvas.height);//clear full canva
 }
